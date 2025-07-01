62d76fc7d54767c77d5238b0d098f9a36c2f990d
Maruthi Prasad <bantumaruthiprasad@gmail.com>
2025-07-01 10:30:35 +0100

    Update login.js

--- login.js
+++ login.js
@@ -8,6 +8,7 @@ const users = [
   { email: "mdaullah86@gmail.com", password: "1986", name: "Ahsan", business: ["stclaire-valentine"] },
   { email: "stclements@phillys.com", password: "1234", name: "St Clements", business: ["phillys-stclements"] },
   { email: "rick@diner.com", password: "1234", name: "Rick", business: ["ricks-diner"] },
+  { email: "dejejus14@yahoo.com", password: "1990", name: "Rick", business: ["ricks-diner"] },
   { email: "toprakcideniz@gmail.com", password: "1995", name: "Rick", business: ["ricks-diner"] },
   { email: "sonaruksha97@gmail.com", password: "1997", name: "Sona", business: ["ricks-diner"] },
   { email: "valentine@stclaire.com", password: "1234", name: "Valentine", business: ["stclaire-valentine"] },
